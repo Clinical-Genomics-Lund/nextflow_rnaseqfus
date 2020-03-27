5f68636a100d54df5ed7c69de8572530273bff3c
sima-r <sima.rahimi@skane.se>
2020-03-27 16:45:08 +0100

    delet

--- run_hopper.sh
+++
@@ -1,3 +0,0 @@
-module load Java singularity nextflow
-nohup nextflow run main.nf --csv ./example.csv -with-singularity /fs1/resources/containers/rnaseqfus_active.sif -with-timeline timeline2.html -with-trace trace2.txt  -resume > out.log
-
