865ed04a9aa7991cfab633b6e11fdfa7e877b83e
sima-r <54620700+sima-r@users.noreply.github.com>
2020-10-20 14:45:08 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,1 @@
-nohup nextflow run main.nf -c ./configs/nextflow.trannel.config --csv ./RnaSeqValidationSamples_filt2.csv -with-singularity container/rnaseqfus_active.sif -with-timeline timeline.html  -resume > run.log &
-#nextflow run main.nf -c ./configs/nextflow.trannel.config --csv ./example.csv -with-singularity container/star-fusion.v1.8.1.simg  -resume
-
+nohup nextflow run main.nf -c ./configs/nextflow.trannel.config --csv /path/to/xxx.csv -with-singularity /path/to/container/rnaseqfus_active.sif -with-timeline timeline.html  -resume > run.log &
